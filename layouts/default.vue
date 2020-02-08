f1c3770879c17e564c79126a364c4a2fa55e5be8
bepsvpt <og7lsrszah6y3lz@infinitefa.email>
2020-02-08 10:48:58 +0800

    let footer background color more dark

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <div class="h-full flex flex-col">
     <nuxt class="container flex-grow px-4 py-6 mx-auto" />
 
-    <footer class="py-3 bg-gray-200 flex justify-center">
+    <footer class="py-3 bg-gray-300 flex justify-center">
       <span>bepsvpt.me</span>
       <span class="mx-1">@</span>
       <span v-text="new Date().getFullYear()" />
