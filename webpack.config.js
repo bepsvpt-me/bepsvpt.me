372c88ac49a5b3680484e3c8aa48158e250738b7
bepsvpt <og7lsrszah6y3lz@infinitefa.email>
2020-05-21 21:03:04 +0800

    remove @ alias

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,8 @@ const path = require('path')
 
 module.exports = {
   resolve: {
-    // for WebStorm
     alias: {
-      '@': path.resolve(__dirname),
-      '~': path.resolve(__dirname)
-    }
-  }
+      '~': path.resolve(__dirname),
+    },
+  },
 }
