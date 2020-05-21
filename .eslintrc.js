8c6355e9363223764e697fd665698af26049fd6d
bepsvpt <og7lsrszah6y3lz@infinitefa.email>
2020-05-21 21:02:42 +0800

    update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,22 +2,15 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
+    node: true,
   },
   extends: [
-    '@nuxtjs',
+    '@nuxtjs/eslint-config-typescript',
     'prettier',
     'prettier/vue',
     'plugin:prettier/recommended',
-    'plugin:nuxt/recommended'
+    'plugin:nuxt/recommended',
   ],
-  plugins: [
-    'prettier'
-  ],
-  // add your custom rules here
-  rules: {
-  }
+  plugins: ['prettier'],
+  rules: {},
 }
