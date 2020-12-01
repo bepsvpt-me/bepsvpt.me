b4911e9ef4e73bb78dd6d6fef599ec87e5ed68ab
bepsvpt <og7lsrszah6y3lz@infinitefa.email>
2020-12-01 08:46:08 +0800

    update footer layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,10 +2,8 @@
   <div class="h-full flex flex-col">
     <nuxt class="container flex-grow px-4 py-6 mx-auto" />
 
-    <footer class="py-3 bg-gray-300 flex justify-center">
-      <span>bepsvpt.me</span>
-      <span class="mx-1">@</span>
-      <span v-text="new Date().getFullYear()" />
+    <footer class="h-8 bg-gray-300 text-center">
+      <a href="https://bepsvpt.me">bepsvpt.me</a>
     </footer>
   </div>
 </template>
